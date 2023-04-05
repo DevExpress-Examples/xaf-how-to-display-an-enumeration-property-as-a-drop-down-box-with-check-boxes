ff06397969a979ba38b980c02016ec2ffa190f4c
andrey <andrey.kozhevnikov@devexpress.com>
2023-04-05 17:16:26 +0400

    init blazor editor

---
+++ CS/XPO/EnumCheckbox/EnumCheckbox.Blazor.Server/Editors/EnumPropertyEditor/EnumPropertyEditor.cs
@@ -0,0 +1,13 @@
+﻿using DevExpress.ExpressApp.Blazor.Editors.Adapters;
+using DevExpress.ExpressApp.Blazor.Editors;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+
+namespace EnumCheckbox.Blazor.Server.Editors.EnumPropertyEditor {
+  
+    [PropertyEditor(typeof(System.Enum), false)]
+    public class EnumPropertyEditor : BlazorPropertyEditorBase {
+        public EnumPropertyEditor(Type objectType, IModelMemberViewItem model) : base(objectType, model) { }
+        protected override IComponentAdapter CreateComponentAdapter() => new EnumAdapter(new EnumEditorModel());
+    }
+}
